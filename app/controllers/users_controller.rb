8304fec8b3b4f68893dfbbb9c49d5255437878ee
Ryan Back <ryanpback@gmail.com>
2020-11-06 23:09:31 -0800

    Add user controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, except: %i[auto_login]
+
+  def register
+    @user = User.new(user_params)
+    @user, errors = RegistrationCompletion.complete_registration(user: @user)
+
+    unless errors.blank?
+      return render json: { error: errors.to_hash(true) }, status: :conflict
+    end
+
+    token = AuthorizationService.encode_token({ user_id: @user.id })
+    response = {
+      message: "User #{@user.username} created successfully",
+      token: token
+    }
+
+    render json: response, status: :created
+  end
+
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user&.authenticate(params[:password])
+      token = AuthorizationService.encode_token({ user_id: @user.id })
+
+      response = {
+        user: {
+          first_name: @user.first_name,
+          last_name: @user.last_name,
+          username: @user.username,
+          emai: @user.email
+        },
+        token: token
+      }
+
+      render json: response, status: :ok
+    else
+      response = { error: 'Invalid username or password' }
+
+      render json: response, status: :unauthorized
+    end
+  end
+
+  def auto_login
+    render json: @current_user, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'register', to: 'users#register'
+  post 'login', to: 'users#login'
+  get 'auto_login', to: 'users#auto_login'
 end
