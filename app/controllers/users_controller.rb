4ea26e27b884d9e25127de2d0ca4638adbca9ff9
Ryan Back <ryanpback@gmail.com>
2020-11-08 14:48:53 -0800

    Add user id to login response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
       response = {
         user: {
+          id: @user.id,
           first_name: @user.first_name,
           last_name: @user.last_name,
           username: @user.username,
