2869388010b13630d84f8ada8e18800c6716c553
Ryan Back <ryanpback@gmail.com>
2020-11-08 21:38:47 -0800

    Superfluous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class ApplicationController < ActionController::API
     @current_user = AuthorizationService.call(request.headers)
 
     # User doesn't exist from user_id in token
-    return render json: { message: 'Please log in' }, status: :unauthorized unless @current_user
-
-    # User in params doesn't match user in token
-    render json: {
-      error: 'Unauthorized'
-    }, status: :unauthorized unless params[:user_id] &&
-      @current_user.id == params[:user_id]
+    render json: { message: 'Please log in' }, status: :unauthorized unless @current_user
   end
 end
