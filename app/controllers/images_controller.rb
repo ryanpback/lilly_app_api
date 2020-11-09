b641d181ab459e17addffcf21f653572df17aaf2
Ryan Back <ryanpback@gmail.com>
2020-11-08 17:09:41 -0800

    Add images controller with method to save image to gcs

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,29 @@
+class ImagesController < ApplicationController
+  def create
+    iv = ImageValidator.validate!(params: params)
+
+    return render json: { error: iv.error }, status: iv.status if iv.error
+
+    bucket      = @current_user.bucket
+    gcs_service = GcsManagementService.new(bucket.id)
+    filename    = gcs_service.upload_file(file: params[:lilly_app_upload])
+    image       = BucketService.new(bucket).save_image!(filename: filename)
+
+    if image.valid?
+      render json: { message: 'File successfully saved' }, status: :created
+    else
+      render json: { error: image.errors.to_hash(true) }, status: :conflict
+    end
+
+  rescue GcsManagementService::BucketExistsError
+    render json: { error: 'Storage location already exists.' }, status: :conflict
+  end
+
+  def show
+    #
+  end
+
+  def destroy
+    #
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   post 'register', to: 'users#register'
   post 'login', to: 'users#login'
   get 'auto_login', to: 'users#auto_login'
+
+  resources :users, only: %i() do
+    resources :images, except: %i(edit, update)
+  end
 end
