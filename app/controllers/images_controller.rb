d31723e45f5a9c0dcd35b3c7d58ebd04c7fe43e6
Ryan Back <ryanpback@gmail.com>
2020-11-18 15:42:51 -0800

    Clean up

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,20 +2,30 @@ class ImagesController < ApplicationController
   def create
     iv = ImageValidator.validate!(params: params)
 
-    return render json: { error: iv.error }, status: iv.status if iv.error
+    return render json: {
+      error: iv.error,
+    },
+      status: iv.status if iv.error
 
     bucket      = @current_user.bucket
     gcs_service = GcsManagementService.new(bucket.id)
     filename    = gcs_service.upload_file(file: params[:lilly_app_upload])
     image       = BucketService.new(bucket).save_image!(filename: filename)
 
-    if image.valid?
-      render json: { message: 'File successfully saved' }, status: :created
-    else
-      render json: { error: image.errors.to_hash(true) }, status: :conflict
-    end
+    return render json: {
+      error: image.errors.to_hash(true),
+    },
+      status: :conflict unless image.valid?
+
+    render json: {
+      message: 'File successfully saved',
+    },
+      status: :created
   rescue GcsManagementService::BucketExistsError
-    render json: { error: 'Storage location already exists.' }, status: :conflict
+    render json: {
+      error: 'Storage location already exists.',
+    },
+      status: :conflict
   end
 
   def show; end
